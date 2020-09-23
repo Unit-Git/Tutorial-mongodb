16c0b536887e893d749075b18ede106d1955cb97
Unit-Git <theunit954@gmail.com>
2020-09-23 17:29:07 +0700

    #6 query Document

---
+++ query.js
@@ -0,0 +1,21 @@
+// db.collection.find(query) //* Mencari doc dgn query
+
+//> select *  from customers where _id = 'khannedy'
+db.customers.find({
+  _id: "khannedy",
+});
+
+//> select * from customers where name = 'Eko Kurniawan Khannedy'
+db.customers.find({
+  name: "Eko Kurniawan Khannedy",
+});
+
+//> select * from products where price = 2000
+db.products.find({
+  price: 2000,
+});
+
+//# select * from orders where items.product_id = 1
+db.orders.find({
+  "items.product_id": 1,
+});
